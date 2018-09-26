7d869b154b6233bc48c6ff01d537274fe86ade77
Fraser LeFevre <mp3lefevre@gmail.com>
2018-09-26 11:16:45 -0600

    Add JS Function

--- web/teach02/teach02.js
+++ web/teach02/teach02.js
@@ -2,3 +2,9 @@ function ClickAlert () {
 	"use strict";
 	window.alert("Clicked!");
 }
+
+function ChangeColor () {
+	"use strict";
+	var color = document.getElementById("color").value;
+	document.getElementById("top_div").style.color = color;
+}
