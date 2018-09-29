ce07b7c447601f0b1967685a8c17816d58e9246f
Fraser LeFevre <mp3lefevre@gmail.com>
2018-09-29 17:29:42 -0600

    Working on Index Page

--- web/index.php
+++ web/index.php
@@ -21,6 +21,7 @@
 	    		<div class="container-fluid">
 	    			<h3 class="h3">A note&#9835; about myself:</h3>
 	    			<p id="intro" class="p-lg-3">My Body</p>
+	    			<img src="pictures/MyWifeAndI.jpg" alt="My Wife And I" class="col-lg">
 	    		</div>
 	    	</div>
 
