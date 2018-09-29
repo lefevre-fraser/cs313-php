2abff89290e9ef300d33c0b85acae23c75392411
Fraser LeFevre <mp3lefevre@gmail.com>
2018-09-29 16:43:31 -0600

    Working on Index Page

--- web/index.php
+++ web/index.php
@@ -11,7 +11,7 @@
     </head>
     
     <body>
-    	<div id="main_div" class="container-fluid border-primary border-light border rounded">
+    	<div id="main_div" class="container-fluid border-primary border-light border rounded-circle">
 
 	    	<?php  
 	    		include("header.php");
