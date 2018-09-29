b6868e0bb79b2e72369ce5bce29449dbef22de27
Fraser LeFevre <mp3lefevre@gmail.com>
2018-09-29 16:07:36 -0600

    Working on Index Page

--- web/header.php
+++ web/header.php
@@ -9,9 +9,9 @@ print(
 			<h1>Welcome to my Home... Page.</h1>
 		</header>
 		<hr>
-		<div>
-			<a class='btn btn-lg btn-primary' href='index.php'>About Me</a>
-			<a class='btn btn-lg btn-primary' href='assignments.php'>Assignments Page</a>
+		<div class='container-fluid'>
+			<a class='btn btn-lg btn-primary btn-group' href='index.php'>About Me</a>
+			<a class='btn btn-lg btn-primary btn-group' href='assignments.php'>Assignments Page</a>
 		</div>
 		<hr>
 	</div>
