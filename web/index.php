c2d91da3e51e252a19f4a4235946212fb14404a8
Fraser LeFevre <mp3lefevre@gmail.com>
2018-09-29 15:10:58 -0600

    Updating Index File

--- web/index.php
+++ web/index.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Title XX</title>
+        <link type="text/css" rel="stylesheet" href="basic.css">
+        <link type="text/css" rel="stylesheet" href="assignXX.css">
+        <script src="assignXX.js"></script>
+    </head>
+    
+    <body>
+    	<?php  
+    		include("header.php");
+    	?>
 
-
-<?php
-include("header.php")
-
-  // phpinfo();
-
-?>
+        <div id="main_div">
+        
+        </div>
+    </body>
+</html>
