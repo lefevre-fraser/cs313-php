de786de3d39a752d8a552a6a1a55b3008ba58208
Fraser LeFevre <mp3lefevre@gmail.com>
2018-09-29 16:28:00 -0600

    Working on Index Page

--- web/index.php
+++ web/index.php
@@ -11,7 +11,7 @@
     </head>
     
     <body>
-    	<div id="main_div" class="container border-primary border">
+    	<div id="main_div" class="container border-primary border-light border">
 
 	    	<?php  
 	    		include("header.php");
