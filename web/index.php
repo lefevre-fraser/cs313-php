6a7f2e10a070ec6a2966ec821f370bf76aa7a81e
Fraser LeFevre <mp3lefevre@gmail.com>
2018-09-29 15:36:35 -0600

    Working on Index Page

--- web/index.php
+++ web/index.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>My Personal Homepage</title>
         <link type="text/css" rel="stylesheet" href="index.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
-        <script src="index.js"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="index.js"></script>
     </head>
     
     <body>
