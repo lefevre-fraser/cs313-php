349e5911a5b5e76a5d8917e477e46d4f72885f1f
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-03 14:42:55 -0600

    Teach 03

--- web/teach03/teach.php
+++ web/teach03/teach.php
@@ -6,9 +6,9 @@
 <body>
 	<form>
 		<input type="text=" name="name">
-		<label>Name</label><br>
+		<label>Name</label><br><br>
 		<input type="text=" name="email">
-		<label>Email</label><br>
+		<label>Email</label><br><br>
 		<div>
 			<input type="radio" name="major">
 			<label>Computer Science</label>
@@ -19,7 +19,7 @@
 			<input type="radio" name="major">
 			<label>Computer Engineering</label>
 		</div>
-		<label>Comments</label>
+		<br><label>Comments</label><br>
 		<textarea></textarea>
 	</form>
 </body>
