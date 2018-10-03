9b30abc4de1a1dc3de0f1a99847cbac4d1cee433
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-03 15:24:12 -0600

    Teach 03

--- web/teach03/display.php
+++ web/teach03/display.php
@@ -12,9 +12,10 @@
 		echo "Name:  " . $_POST["name"] . "<br>";
 		echo "Email: " . "<a href='mailto:" . $_POST["email"] . "'>" . $_POST["email"] . "</a><br>";
 		echo "Major: " . $_POST["major"] . "<br>";
-		echo "Comments: " . $_POST["comments"] . "<br>";
+		echo "Comments:"
+		echo $_POST["comments"] . "<br>";
 
-		echo "Continets:<br>";
+		echo "Continets:";
 		foreach ($_POST["continents"] as $key => $value) {
 			echo $value . "<br>";
 		}
