08f6d80313b5860f36d605ae2a719003f1bec47c
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-03 15:08:07 -0600

    Teach 03

--- web/teach03/teach03.php
+++ web/teach03/teach03.php
@@ -20,19 +20,19 @@
 			<label>Computer Engineering</label>
 		</div>
 		<div>
-			<input type="checkbox" name="continents">
+			<input type="checkbox" value="North America" name="continents">
 			<label>North America</label>
-			<input type="checkbox" name="continents[]">
+			<input type="checkbox" value="South America" name="continents[]">
 			<label>South America</label>
-			<input type="checkbox" name="continents[]">
+			<input type="checkbox" value="Europe" name="continents[]">
 			<label>Europe</label>
-			<input type="checkbox" name="continents[]">
+			<input type="checkbox" value="Asia" name="continents[]">
 			<label>Asia</label>
-			<input type="checkbox" name="continents[]">
+			<input type="checkbox" value="Austrailia" name="continents[]">
 			<label>Austrailia</label>
-			<input type="checkbox" name="continents[]">
+			<input type="checkbox" value="Africa" name="continents[]">
 			<label>Africa</label>
-			<input type="checkbox" name="continents[]">
+			<input type="checkbox" value="Antartica" name="continents[]">
 			<label>Antartica</label>
 		</div>
 		<br><label>Comments</label><br>
