150c59853bd963f7c8b6e267106710412cce967f
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-17 17:46:51 -0600

    Asset Tracker

--- web/AssetsTracking/login.php
+++ web/AssetsTracking/login.php
@@ -3,10 +3,12 @@
 
 	$user_name = $db->query("select user_id from users where user_name = '" . $_POST["user_name"] . "'");
 
-	if (isset($user_name["user_id"])) {
+	echo $user_name;
+
+	// if (isset($user_name["user_id"])) {
 		// session_start();
 		// $_SESSION["user_name"] = $user_name["user_name"];
-	}
+	// }
 
 	// echo $_SESSION["user_name"];
 ?>
