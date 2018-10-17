41f20bf69f9d211e6543cbcab197ce5dc0c8add5
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-17 17:53:15 -0600

    Asset Tracker

--- web/AssetsTracking/login.php
+++ web/AssetsTracking/login.php
@@ -1,12 +1,13 @@
 <?php
 	include("DatabaseConnect.php");
 
-	$user_name = $db->query("select user_id from users where user_name = '" . $_POST["user_name"] . "'");
-
-	echo gettype($user_name);
+	$user_name = $db->query("select user_name from users where user_name = '" . $_POST["user_name"] . "'");
 
 	foreach ($user_name as $row) {
-		echo $row["user_id"];
+		if (isset($row["user_name"])) {
+			session_start();
+			$_SESSION["user_name"] = $row["user_name"];
+		}
 	}
 
 	// echo $user_name;
