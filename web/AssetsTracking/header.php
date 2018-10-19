75e6374bed8edd05fa81865df01862e85f79c91f
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-19 15:34:51 -0600

    Asset Tracker

--- web/AssetsTracking/header.php
+++ web/AssetsTracking/header.php
@@ -3,7 +3,7 @@
 $header =  "<h1>Name: " . $_SESSION["full_name"] . "</h1>";
 
 $header .= "<a href='AssetTracker.php' id='tracker' class='btn btn-primary'>Asset Tracker Page</a>";
-$header .= "<a href='AssetInertForm.php' id='insert' class='btn btn-primary'>Insert Asset Page</a>";
+$header .= "<a href='AssetInsertForm.php' id='insert' class='btn btn-primary'>Insert Asset Page</a>";
 $header .= "<a href='logout.php' class='btn btn-secondary'>Log Out</a>";
 
 $header .= "<br><hr>";
