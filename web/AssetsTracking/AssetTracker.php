61c3f25b0bf6b96f8c35e25bb217875a95c19352
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-19 14:14:58 -0600

    Asset Tracker

--- web/AssetsTracking/AssetTracker.php
+++ web/AssetsTracking/AssetTracker.php
@@ -7,7 +7,12 @@ session_start();
 <head>
 	<title>Asset Tracker</title>
 
-	<?php include_once("BootStrapHeader.php"); ?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 	<link rel="stylesheet" type="text/css" href="AssetTracker.css">
 	<script type="text/javascript" src="AssetTracker.js"></script>
@@ -85,7 +90,8 @@ session_start();
 
 	</div>
 
-	<?php include_once("BootStrapFooter.php"); ?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 </html>
