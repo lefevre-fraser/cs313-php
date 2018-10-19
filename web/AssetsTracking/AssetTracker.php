7cc348678eb7f1be24d7037af5bc3cc5c7783fe3
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-19 14:18:44 -0600

    Asset Tracker

--- web/AssetsTracking/AssetTracker.php
+++ web/AssetsTracking/AssetTracker.php
@@ -7,12 +7,10 @@ session_start();
 <head>
 	<title>Asset Tracker</title>
 
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<?php 
+	include_once("BootStrapHeader.php"); 
+	echo $BootStrapHeader;
+	?>
 
 	<link rel="stylesheet" type="text/css" href="AssetTracker.css">
 	<script type="text/javascript" src="AssetTracker.js"></script>
@@ -22,13 +20,9 @@ session_start();
 
 		<?php 
 
-		if (isset($_SESSION["login"]) && $_SESSION["login"])) {
-			echo '<div id="main_div" class="container">';
-
-			echo "<h1>Name: " . $_SESSION["full_name"] . "</h1>";
-			echo "<a href='logout.php' class='btn btn-secondary'>Log Out</a>";
-
-			echo "<br><hr>";
+		if (isset($_SESSION["login"]) && $_SESSION["login"]) {
+			include_once("header.php");
+			echo $header;
 
 			echo "<form action='AssetTracker.php' method='post'>";
 			echo "<label>Search by Asset Name:</label><br>";
@@ -90,8 +84,10 @@ session_start();
 
 	</div>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<?php 
+	include_once("BootStrapFooter.php"); 
+	echo $BootStrapFooter;
+	?>
 
 </body>
 </html>
