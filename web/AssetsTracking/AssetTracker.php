15321291b0e2fee225d9b7229163e745d7b9e18f
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-19 08:05:29 -0600

    Asset Tracker

--- web/AssetsTracking/AssetTracker.php
+++ web/AssetsTracking/AssetTracker.php
@@ -24,13 +24,13 @@ session_start();
 			echo "<h1>User Name: " . $_SESSION["user_name"] . "</h1>";
 			echo "<a href='logout.php' class='btn btn-secondary'>Log Out</a>";
 
-			echo "<br><hr><br>";
+			echo "<br><hr>";
 
 			echo "<form action='AssetTracker.php' method='post'>";
 			echo "<label>Search by Asset Name:</label><br>";
 			echo "<input name='search_context' type='text' placeholder='Couch'>";
-			echo "<button type='submit'>Search</button><br>";
-			echo "</form>";
+			echo "<button type='submit'>Search</button>";
+			echo "</form><br>";
 
 			include("DatabaseConnect.php");
 
