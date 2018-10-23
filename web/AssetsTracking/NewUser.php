bd139531a07f67fb40cf36385c61edd2fd1015bf
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-23 17:13:18 -0600

    Asset Tracker

--- web/AssetsTracking/NewUser.php
+++ web/AssetsTracking/NewUser.php
@@ -1,11 +1,12 @@
 <?php
 include_once("DatabaseConnect.php");
 
-$queryString  = "select salt_id, salt_value from salts";
-$query = $db->prepare($queryString);
+$queryString  = "select user_name from users";
+$queryString .= " where user_name = 'LeFeve'";
+
+$query       = $db->prepare($queryString);
 $query->execute();
 $queryResult = $query->fetchAll();
-$i = mt_rand(0, sizeof($queryResult) - 1);
-echo $queryResult[$i]["salt_id"];
-echo $queryResult[$i]["salt_value"];
+
+echo sizeof($queryResult);
 ?>
--- web/AssetsTracking/insert.php
+++ web/AssetsTracking/insert.php
@@ -5,24 +5,29 @@ include_once("DatabaseConnect.php");
 
 if (isset($_POST["new_user_name"])) {
 
-	$queryString  = "select salt_id, salt_value from salts";
-	$query = $db->prepare($queryString);
+	$queryString  = "select user_name from users";
+	$queryString .= " where user_name = '" . $_POST["new_user_name"] . "'";
+
+	$queryString    = "select salt_id, salt_value from salts";
+	$query          = $db->prepare($queryString);
 	$query->execute();
-	$queryResult = $query->fetchAll();
-	$i = mt_rand(0, sizeof($queryResult));
-	$queryResult[$i]["salt_id"];
+	$queryResult    = $query->fetchAll();
+	$i              = mt_rand(0, sizeof($queryResult) - 1);
+
+	$options        = [ 'cost' => 8, 'salt' => $queryResult[$i]["salt_value"]];
+	$HashedPassword = password_hash($_POST["password"], PASSWORD_BCRYPT, $options);
 
 	$queryString  = "select insert_user(";
 	$queryString .= "'" . $_POST["new_user_name"] . "'";
-	$queryString .= "'" . $_POST["fname"] . "'";
-	$queryString .= "'" . $_POST["lname"] . "'";
-	$queryString .= "'" . $_POST["area_code"] . "'";
-	$queryString .= "'" . $_POST["phone_number"] . "'";
-	$queryString .= $saltId;
-	$queryString .= $HashedPassword;
+	$queryString .= ", '" . $_POST["fname"] . "'";
+	$queryString .= ", '" . $_POST["lname"] . "'";
+	$queryString .= ", '" . $_POST["area_code"] . "'";
+	$queryString .= ", '" . $_POST["phone_number"] . "'";
+	$queryString .= ", " . $saltId;
+	$queryString .= ", " . $HashedPassword;
 
 	if (isset($_POST["mname"])) {
-		$queryString .= "'" . $_POST["mname"] . "'";
+		$queryString .= ", '" . $_POST["mname"] . "'";
 	}
 
 	$queryString .= ")";
