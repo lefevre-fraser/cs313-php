9c5b3ae8bef2a1743677368d3c343ce47c02269a
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-23 16:00:56 -0600

    Asset Tracker

--- web/AssetsTracking/NewUser.php
+++ web/AssetsTracking/NewUser.php
@@ -3,9 +3,9 @@ include_once("DatabaseConnect.php");
 
 $queryString  = "select salt_id, salt_value from salts";
 
-$queryResult = $db->prepare($queryString);
-$queryResult->execute();
-$queryResult = $queryResult->fetch_all();
+$query = $db->prepare($queryString);
+$query->execute();
+$queryResult = $query->fetch_all();
 
 print_r($queryResult);
 ?>
