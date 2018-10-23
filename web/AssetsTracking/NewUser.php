6e79e90fc059344eee2bff92e7b98d7714b07034
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-23 15:29:39 -0600

    Asset Tracker

--- web/AssetsTracking/NewUser.php
+++ web/AssetsTracking/NewUser.php
@@ -6,7 +6,6 @@ $queryString  = "select salt_id, salt_value from salts";
 $queryResult = $dq->query($queryString);
 
 foreach ($queryResult as $key => $value) {
-	echo $key;
-	echo $value;
+	echo gettype($key);
 }
 ?>
