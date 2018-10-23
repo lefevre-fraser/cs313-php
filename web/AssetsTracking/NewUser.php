23a1340c0d037bcc07129b366e2b91d7d15df202
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-23 15:26:49 -0600

    Asset Tracker

---
+++ web/AssetsTracking/NewUser.php
@@ -0,0 +1,11 @@
+<?php
+include_once("DatabaseConnect.php");
+
+$queryString  = "select salt_id, salt_value from salts";
+
+$queryResult = $dq->query($queryString);
+
+foreach ($queryResult as $key => $value) {
+	echo $key;
+}
+?>
--- web/AssetsTracking/insert.php
+++ web/AssetsTracking/insert.php
@@ -3,15 +3,43 @@ session_start();
 
 include_once("DatabaseConnect.php");
 
-$queryString  = "select insert_asset(";
-$queryString .= "'" . $_SESSION["user_name"] . "'";
-$queryString .= ", " . $_POST["quantity"];
-$queryString .= ", " . $_POST["asset_value"];
-$queryString .= ", '" . $_POST["asset_name"] . "'";
-$queryString .= ")";
+if (isset($_POST["new_user_name"])) {
 
-$completed = $db->query($queryString);
+	$queryString  = "select salt_id, salt_value from salts";
 
-header("Location: AssetInsertForm.php");
-exit;
+	$queryString  = "select insert_user(";
+	$queryString .= "'" . $_POST["new_user_name"] . "'";
+	$queryString .= "'" . $_POST["fname"] . "'";
+	$queryString .= "'" . $_POST["lname"] . "'";
+	$queryString .= "'" . $_POST["area_code"] . "'";
+	$queryString .= "'" . $_POST["phone_number"] . "'";
+	$queryString .= $saltId;
+	$queryString .= $HashedPassword;
+
+	if (isset($_POST["mname"])) {
+		$queryString .= "'" . $_POST["mname"] . "'";
+	}
+
+	$queryString .= ")";
+
+	$completed = $db->query($queryString);
+
+	header("Location: AssetTracker.php");
+	exit;
+
+} else {
+
+	$queryString  = "select insert_asset(";
+	$queryString .= "'" . $_SESSION["user_name"] . "'";
+	$queryString .= ", " . $_POST["quantity"];
+	$queryString .= ", " . $_POST["asset_value"];
+	$queryString .= ", '" . $_POST["asset_name"] . "'";
+	$queryString .= ")";
+
+	$completed = $db->query($queryString);
+
+	header("Location: AssetInsertForm.php");
+	exit;
+
+}
 ?>
