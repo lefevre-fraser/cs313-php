749e11d674ee64d589e157597a5b65cf9d03bb25
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-24 10:33:39 -0600

    Asset Tracker

--- web/AssetsTracking/testing.php
+++ web/AssetsTracking/testing.php
@@ -2,9 +2,13 @@
 echo $_GET["phone_number"];
 
 $area_code = preg_split("/(/", $_GET["phone_number"]);
-print_r($area_code);
+
+echo $area_code[0];
+echo "<hr>";
+echo $area_code[1];
+
 $area_code = preg_split("/)/", $area_code[0]);
-print_r($area_code);
+
 
 echo $area_code[0];
 ?>
