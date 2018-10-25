21d5383b08ae9fcf0ccca029e45f08de393dcc52
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-25 13:48:01 -0600

    Asset Tracker

--- web/AssetsTracking/header.php
+++ web/AssetsTracking/header.php
@@ -2,9 +2,11 @@
 
 $header =  "<h1>Name: " . $_SESSION["full_name"] . "</h1>";
 
+$header .= "<div class='d-flex flex-row'>";
 $header .= "<a href='AssetTracker.php' id='tracker' class='btn btn-primary'>Asset Tracker</a>";
 $header .= "<a href='AssetInsertForm.php' id='insert' class='btn btn-primary'>Asset Insert</a>";
-$header .= "<a href='logout.php' class='btn btn-primary justify-content-end'>Log Out</a>";
+$header .= "<a href='logout.php' class='btn btn-primary ml-auto'>Log Out</a>";
+$header .= "</div>";
 
 $header .= "<br><hr>";
 
--- web/AssetsTracking/testing.php
+++ web/AssetsTracking/testing.php
@@ -1,18 +1,10 @@
 <?php
-session_start();
 
 include_once("BootStrapHeader.php");
 echo $BootStrapHeader;
 
-$_SESSION["insert_user_error"] = 2;
-$_SESSION["insert_error"] = 2;
-$_SESSION["change_error"] = 3;
-
-echo "stuff";
-
-include_once("ErrorDictionary.php");
-
-insert_error_scripts();
+include_once("header.php");
+echo $header;
 
 include_once("BootStrapFooter.php");
 echo $BootStrapFooter;
