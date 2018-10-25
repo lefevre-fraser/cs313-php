cc70cb0a4f503aefb2343f9f0ab07658446e15c9
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-25 16:10:42 -0600

    Asset Tracker

--- web/AssetsTracking/edit.php
+++ web/AssetsTracking/edit.php
@@ -14,6 +14,9 @@ if (isset($_POST["update"])) {
 		$asset_id = $asset[0];
 		$old_asset_value = $asset[1];
 
+		echo $asset_id;
+		echo $old_asset_value;
+
 		$asset = $_POST[$asset_identify];
 
 		$queryString  = "delete from user_assets";
@@ -23,7 +26,7 @@ if (isset($_POST["update"])) {
 
 		$query = $db->prepare($queryString);
 		$query->execute();
-		print_r($query->fetchAll());
+		echo json_encode($query->fetchAll());
 	}
 
 }
