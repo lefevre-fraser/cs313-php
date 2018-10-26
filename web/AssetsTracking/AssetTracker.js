f25cc773d0d92ccf8b25f675c0442d5d673107a2
Fraser LeFevre <mp3lefevre@gmail.com>
2018-10-26 08:40:11 -0600

    Asset Tracker

--- web/AssetsTracking/AssetTracker.js
+++ web/AssetsTracking/AssetTracker.js
@@ -5,3 +5,7 @@ function setActive(id) {
 function check(id) {
 	document.getElementById(id).checked = true;
 }
+
+function confirmDelete() {
+	return confirm("Are you sure you want to delete the selected items?");
+}
